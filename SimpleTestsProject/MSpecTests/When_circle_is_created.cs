209e77eee5cdb9d3973b07347441d4ce1e672033
AlexeyTotin <Alexey.Totin@jetbrains.com>
2018-02-15 15:17:52 +0100

    add MSpec tests

---
+++ SimpleTestsProject/MSpecTests/When_circle_is_created.cs
@@ -0,0 +1,22 @@
+﻿using Machine.Specifications;
+using SimpleTestsProject;
+
+namespace MSpecTests
+{
+    [Subject(typeof(Circle))]
+    public class When_circle_is_created
+    {
+        private static Point center;
+        private static Circle circle;
+
+        private Establish context = () =>
+        {
+            center = new Point(0, 0);
+            circle = new Circle(center, 10);
+        };
+
+        private It should_correctly_calculate_square = () => circle.GetSquare().ShouldEqual(314); 
+
+        private It should_correctly_calculate_length = () => circle.GetLength().ShouldEqual(62.8); 
+    }
+}
